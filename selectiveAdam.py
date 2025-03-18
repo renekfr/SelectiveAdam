d2c1537f1000c9f841dc0bee99c6373ce2c431bd
renekfr <162452393+renekfr@users.noreply.github.com>
2025-03-18 17:51:08 +0100

    Small fix, contiguous memory.

--- selectiveAdam.py
+++ selectiveAdam.py
@@ -7,11 +7,16 @@ class SelectiveAdam(torch.optim.Optimizer):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         super().__init__(params, defaults)
 
-    def step(self, visibility=None):
+    def step(self, closure=None, mask=None):   
+        if not mask.any():
+            return
+        
+        mask = mask.nonzero(as_tuple=True)[0] 
+           
         for group in self.param_groups:
             assert len(group["params"]) == 1, "more than one tensor in group"
-
             param = group["params"][0]
+            
             if param.grad is None:
                 continue
 
@@ -25,16 +30,11 @@ class SelectiveAdam(torch.optim.Optimizer):
                 state["exp_avg"] = torch.zeros_like(param, memory_format=torch.preserve_format)
                 state["exp_avg_sq"] = torch.zeros_like(param, memory_format=torch.preserve_format)
 
+            state["step"] += 1
+
             exp_avg = state["exp_avg"]
             exp_avg_sq = state["exp_avg_sq"]
-
-            state["step"] += 1
             step = state["step"]
-
-            if visibility is None:
-                mask = torch.ones_like(param, dtype=torch.bool)
-            else:
-                mask = visibility.bool()
 
             grad = param.grad[mask]
             if group["weight_decay"] != 0:
@@ -42,13 +42,11 @@ class SelectiveAdam(torch.optim.Optimizer):
 
             exp_avg[mask] = beta1 * exp_avg[mask] + (1 - beta1) * grad
             exp_avg_sq[mask] = beta2 * exp_avg_sq[mask] + (1 - beta2) * (grad ** 2)
-
             bias_1 = 1 - beta1 ** step
             bias_2 = 1 - beta2 ** step
             step_size = lr * (bias_2 ** 0.5) / bias_1
 
             update = exp_avg[mask] / exp_avg_sq[mask].sqrt().add_(eps)
+            param.data[mask] -= step_size * update
 
-            param.data[mask] += -step_size * update
-
-        return None
+        return
